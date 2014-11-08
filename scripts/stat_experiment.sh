ee1d37ac7bcbbb2d0027823d6c0d6850815bb639
Dustin Webb <daemonmaker@gmail.com>
2014-11-08 11:58:53 -0500

    Added script to summarize current experiment from log files.

---
+++ scripts/stat_experiment.sh
@@ -0,0 +1,1 @@
+cat basic.log | grep High && cat basic.log | grep -b1 High | tail -3 && cat basic.log | grep Episode | tail -1 && cat basic.log | grep Frames | tail -1 && cat basic.log | grep Epsilon | tail -1
